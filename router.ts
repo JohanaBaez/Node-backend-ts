9d628fa234ab5114cb289c03579672730bebda3c
JohanaBaez <johanarabaez617@gmail.com>
2022-12-12 19:37:07 -0600

    eliminacion de archivos inecesarios para el proyecto

--- router.ts
+++ router.ts
@@ -1,9 +1,7 @@
 import { Express } from 'express';
 import authRouter from './auth/router';
-import productRouter from './products/router';
 import categoriesRouter from './categories/router';
-import recipesRouter from './recipes/router';
-import creatorsRouter from './creators/router';
+
 
 const router = (app: Express) => {
     app.get("/", (req, res) => {
@@ -12,10 +10,8 @@ const router = (app: Express) => {
         });
     });
     app.use("/auth", authRouter);
-    app.use("/products", productRouter);
     app.use("/categories", categoriesRouter);
-    app.use("/recipes", recipesRouter);
-    app.use("/creators", creatorsRouter);
+
 }
 
 export default router;
